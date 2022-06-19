f5589d75d82e896980a5e7da14891b3db18a692f
Andrzej Skrzynski <andrzej.skrzynski@ihsmarkit.com>
2022-06-19 13:11:34 +0200

    add install script

---
+++ .dotfiles/.install.sh
@@ -0,0 +1,23 @@
+git clone --bare https://github.com/Eandrju/dotfiles $HOME/.dotfiles
+function config {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+config checkout
+if [ $? != 0 ]; then
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 |
+        egrep "\s+\." |
+        awk {'print $1'} |
+        xargs -I% sh -c "mkdir -p '.dotfiles-backup/%';  mv % .dotfiles-backup/%"
+fi;
+
+config checkout
+if [ $? == 0 ]; then
+    echo "Successfully backed up conflicting dotfiles in .dotfiles-backup/. and imported dotfiles.";
+else
+    echo "Mission failed.";
+fi;
+
+config config status.showUntrackedFiles no
+git config --global include.path "~/.gitconfig.aliases"
+
