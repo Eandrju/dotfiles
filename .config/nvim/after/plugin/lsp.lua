0165d30c60e53d640fe812818ba218e90a37bd51
Andrzej Skrzyński <and.skrzynski@gmail.com>
2022-08-30 23:13:36 +0200

    add lsp for python

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -46,3 +46,19 @@ require'lspconfig'.gopls.setup{
         vim.keymap.set('n', '<leader> ', vim.lsp.buf.code_action, {buffer=0})
     end,
 }
+
+-- Python
+require'lspconfig'.pyright.setup{
+    capabilities = capabilities,
+    on_attach = function ()
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
+        vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer=0})
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer=0})
+        vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, {buffer=0})
+        vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, {buffer=0})
+        vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', {buffer=0})
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {buffer=0})
+        vim.keymap.set('n', '<leader> ', vim.lsp.buf.code_action, {buffer=0})
+    end,
+}
