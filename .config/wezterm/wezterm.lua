c19a14f66731774038fb2f343c55e3211ecbb41f
Andrzej Skrzyński <and.skrzynski@gmail.com>
2023-02-11 14:24:09 +0100

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,49 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
+
+return {
+  ---------- WINDOW LAYOUT ----------  
+  window_background_opacity = 0.85,
+  window_decorations = "RESIZE",
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    top = 40,
+    bottom = 0,
+  },
+
+  ----------    FONTS   -----------
+  font_size = 12.5,
+
+  ---------- COLORSCHEME ----------  
+  force_reverse_video_cursor = true,
+  colors = {
+    foreground = '#ebdbb2',
+    background = '#252525',
+
+    ansi = {
+      '#282828',
+      '#cc241d',
+      '#98971a',
+      '#d79921',
+      '#458588',
+      '#b16286',
+      '#689d6a',
+      '#a89984',
+    },
+    brights = {
+      '#928373',
+      '#fb4934',
+      '#b8bb26',
+      '#fabd2f',
+      '#83a598',
+      '#d3869b',
+      '#8ec07c',
+      '#ebdbb2',
+    },
+  }
+}
